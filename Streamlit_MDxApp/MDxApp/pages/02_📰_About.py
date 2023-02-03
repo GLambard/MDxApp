1371811ea5a1d7189edd0c6c748e4a6cd847cff8
GLambard <geonosis.lambard@gmail.com>
2023-02-03 16:02:08 +0900

    Directory name change

---
+++ Streamlit_MDxApp/MDxApp/pages/02_📰_About.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About",
+    page_icon="📰",
+)
+
+st.header("About")
+
+#st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    ### **Brief description of the web app**
+    This app is designed to assist medical doctors in finding a 
+    right diagnostic by allowing them to enter relevant information such as age, 
+    gender, pregnancy state, environmental context, symptoms, observations, and 
+    pre-treatment information.
+    ### Versions
+    :sparkles: Current version: V1.01 (2023/01/30) :sparkles:
+    ### :rotating_light: **Caution message** :rotating_light:
+    Please be aware that while the app is designed to assist medical professionals, 
+    the final diagnosis should be made by a licensed medical professional. We recommend 
+    seeking additional evaluations and opinions before making any treatment decisions.
+    ### **Message from the developer**
+    > Dear community,  
+    > 
+    > I am proud to announce the deployment of this free app that provides medical diagnosis assistance to those in need.  
+    > 
+    > I want to take this moment to thank the open-source community for their unwavering support in making this project a reality. 
+    > Your contributions, in any form, have allowed me to bring this tool to all.  
+    > 
+    > I hope this app will positively impact people's lives, and I am grateful for the opportunity to serve the community in this way.  
+    > 
+    > Thank you for your continued support.  
+    > 
+    > Best,  
+    > 
+    > Guillaume Lambard  
+    > AI solutions designer and developer  
+    > *(who developed this tool in his spare time)*
+    >  
+    >  
+    """, 
+    unsafe_allow_html=True
+)
